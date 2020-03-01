a84737e4c5d18f352f2f8991ce29367b2d974dd5
kekeing <kekeing2016@gmail.com>
2020-03-01 23:55:45 +0800

    new file:   app/KcM.py new file:   app/splice_clustering.py new file:   notebook/data_merge.ipynb new file:   notebook/kmean.ipynb modified:   requirements.txt

---
+++ app/KcM.py
@@ -0,0 +1,98 @@
+
+import time
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.cluster import MiniBatchKMeans, KMeans
+from sklearn.metrics.pairwise import pairwise_distances_argmin
+
+
+batch_size = 300
+df = pd.read_csv(
+    r'/home/kekeing/Desktop/code/DateMining/data/lianjia_processed.csv', sep=',')
+df = df[['deal_totalPrice', 'gross_area']]
+da = df.to_numpy()
+n_clusters = 3
+# #############################################################################
+# Compute clustering with Means
+
+k_means = KMeans(init='k-means++', n_clusters=n_clusters, n_init=10)
+t0 = time.time()
+k_means.fit(da)
+t_batch = time.time() - t0
+
+# #############################################################################
+# Compute clustering with MiniBatchKMeans
+
+mbk = MiniBatchKMeans(init='k-means++', n_clusters=n_clusters, batch_size=batch_size,
+                      n_init=10, max_no_improvement=10, verbose=0)
+t0 = time.time()
+mbk.fit(da)
+t_mini_batch = time.time() - t0
+
+# #############################################################################
+# Plot result
+
+fig = plt.figure(figsize=(8, 3))
+fig.subplots_adjust(left=0.02, right=0.98, bottom=0.05, top=0.9)
+colors = ['#4EACC5', '#FF9C34', '#4E9A06']
+
+# We want to have the same colors for the same cluster from the
+# MiniBatchKMeans and the KMeans algorithm. Let's pair the cluster centers per
+# closest one.
+k_means_cluster_centers = k_means.cluster_centers_
+order = pairwise_distances_argmin(k_means.cluster_centers_,
+                                  mbk.cluster_centers_)
+mbk_means_cluster_centers = mbk.cluster_centers_[order]
+
+k_means_labels = pairwise_distances_argmin(da, k_means_cluster_centers)
+mbk_means_labels = pairwise_distances_argmin(da, mbk_means_cluster_centers)
+print(k_means_labels)
+# KMeans
+ax = fig.add_subplot(1, 3, 1)
+for k, col in zip(range(n_clusters), colors):
+    my_members = k_means_labels == k
+    cluster_center = k_means_cluster_centers[k]
+    ax.plot(da[my_members, 0], da[my_members, 1], 'w',
+            markerfacecolor=col, marker='.')
+    ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+            markeredgecolor='k', markersize=6)
+ax.set_title('KMeans')
+ax.set_xticks(())
+ax.set_yticks(())
+plt.text(-3.5, 1.8,  'train time: %.2fs\ninertia: %f' % (
+    t_batch, k_means.inertia_))
+
+# MiniBatchKMeans
+ax = fig.add_subplot(1, 3, 2)
+for k, col in zip(range(n_clusters), colors):
+    my_members = mbk_means_labels == k
+    cluster_center = mbk_means_cluster_centers[k]
+    ax.plot(da[my_members, 0], da[my_members, 1], 'w',
+            markerfacecolor=col, marker='.')
+    ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+            markeredgecolor='k', markersize=6)
+ax.set_title('MiniBatchKMeans')
+ax.set_xticks(())
+ax.set_yticks(())
+plt.text(-3.5, 1.8, 'train time: %.2fs\ninertia: %f' %
+         (t_mini_batch, mbk.inertia_))
+
+# Initialise the different array to all False
+different = (mbk_means_labels == 4)
+ax = fig.add_subplot(1, 3, 3)
+
+for k in range(n_clusters):
+    different += ((k_means_labels == k) != (mbk_means_labels == k))
+
+identic = np.logical_not(different)
+ax.plot(da[identic, 0], da[identic, 1], 'w',
+        markerfacecolor='#bbbbbb', marker='.')
+ax.plot(da[different, 0], da[different, 1], 'w',
+        markerfacecolor='m', marker='.')
+ax.set_title('Difference')
+ax.set_xticks(())
+ax.set_yticks(())
+
+plt.savefig("mygraph.png")
---
+++ app/splice_clustering.py
@@ -0,0 +1,34 @@
+
+import pandas as pd
+
+from sklearn.cluster import KMeans
+
+if __name__ == '__main__':
+
+    df = pd.read_csv(
+    r'/home/kekeing/Desktop/code/DateMining/data/lianjia_processed.csv', sep=',')
+    dff = df[['deal_totalPrice', 'gross_area']]
+    da = dff.to_numpy()
+
+
+    k_means = KMeans(init='k-means++', n_clusters=3, n_init=22)
+    k_means.fit(da)
+    res0Series = pd.Series(k_means.labels_)
+
+    res0 = res0Series[res0Series.values == 0]
+    res1 = res0Series[res0Series.values == 1]
+    res2 = res0Series[res0Series.values == 2]
+    df_one = dff.iloc[res0.index]
+    df_two = dff.iloc[res1.index]
+    df_three = dff.iloc[res2.index]
+
+    result_one = pd.merge(df_one, df, how='left',on=['deal_totalPrice', 'gross_area'])
+    result_one.to_csv("../data/clustering_one.csv", index=False)
+
+    result_two = pd.merge(df_two, df, how='left',on=['deal_totalPrice', 'gross_area'])
+    result_two.to_csv("../data/clustering_two.csv", index=False)
+
+    result_three = pd.merge(df_two, df, how='left',on=['deal_totalPrice', 'gross_area'])
+    result_three.to_csv("../data/clustering_three.csv", index=False)
+
+
